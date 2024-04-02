410b675ac290a14a9a77829a9d094bced54077b4
andylin9 <andygithub90@gmail.com>
2024-04-02 18:07:36 +0800

    fix landing page bg color

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,11 +4,11 @@ import { Heroes } from "./_components/heros";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
