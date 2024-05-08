5cf43ccb749df71099fb712b19f65c9eab5fe780
andylin9 <andygithub90@gmail.com>
2024-05-08 14:30:44 +0800

    doc: 新增文件: 如何用遞迴找到所有文件

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -25,14 +25,6 @@ export const DocumentList = ({
   const router = useRouter();
   const [expanded, setExpanded] = useState<Record<string, boolean>>({});
 
-  // debug 用，看 expanded state
-  // useEffect(() => {
-  //   console.log(expanded);
-  // }, []);
-  // useEffect(() => {
-  //   console.log(expanded);
-  // }, [expanded]);
-
   const onExpand = (documentId: string) => {
     setExpanded((prevExpanded) =>
       // return obj
@@ -80,6 +72,19 @@ export const DocumentList = ({
       >
         No pages inside
       </p>
+      {/* debug 用 */}
+      {console.log("parentDocumentId: ", parentDocumentId)}
+      {console.log("documents: ", documents)}
+      {console.log("expanded: ", expanded)}
+      {/* 
+        documents table 是一個自關聯的模型，最上層的節點的 parentDocument 為 undefined ，如果這個節點的 id 是 123 則他下層節點的 parentDocument field 會記錄 123 ，表示 id 123 是他的父節點
+
+        所以要拿到這個節點下的所有節點要 query parentDocument 為 undefined 的 record ，拿到這個 record 的 id ，假如這個 id 是 123 ，就再 query parentDocument 為 123 的 record
+
+        實作出來就會是先 call DocumentList function component ，傳入的 parentDocumentId 是 undefined ，因為我們要拿到所有最上層的節點，拿到之後再用 map 遍歷所有最上層的節點，再用 recursive 的方式在 DocumentList function component 裡面 call DocumentList function component ，傳入的 parentDocumentId 是最上層節點的 id ， query documents table 找到 parentDocument 是這個 id 的所有記錄，這就會是這個 id 的下一層節點，也就是第二層節點，因為下一層節點的 parentDocument 欄位會記錄父層節點的 id ，再用 map 遍歷這層節點，把這層節點的 id 拿去query documents table 找到 parentDocument 是這個 id 的所有記錄，也就是 map 遍歷這層節點的下層節點，通過這種方式不斷去找找下一層節點直到沒有資料就表示已經找到最下層了，也就遍歷完所有文件了。
+
+        但是這邊有做一些優化，沒有直接遍歷完所有好貸節點，只遍歷被展開的下一層子節點，因為我們只需要知道被展開的文件下有什麼子節點，不需要知道他的所有後代節點
+      */}
       {documents.map((document) => (
         <div key={document._id}>
           <Item
